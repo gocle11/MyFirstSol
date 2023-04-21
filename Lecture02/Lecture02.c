f29a6cc7175fa10342eac705411411d788022e6f
LeeJeun <gocle11@gmail.com>
2023-04-21 16:27:25 +0900

    dd

--- Lecture02/Lecture02.c
+++ Lecture02/Lecture02.c
@@ -1,23 +1,30 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
 
-void say_hello(void);
+#include <stdio.h>
+
+//void say_hello(void);
 
 int main()
 {
-    int x, y, z;
+    //int x, y, z;
 
-    x = 1;
-    y = 20;
-    z = 3;
+    //x = 1;
+    //y = 20;
+    //z = 3;
 
-    z = x + y;
+    //z = x + y;
 
-    say_hello();
+    //say_hello();
+
+    int i = 0, j = 0;
+    scanf("%d%d", &i,&j);
+
+    printf("value is %d\n", i);
 
     return 0;
 }
-void say_hello(void) {
-    printf("Hello, World!\n");
-}
+//void say_hello(void) {
+//    printf("Hello, World!\n");
+//}
 
 
