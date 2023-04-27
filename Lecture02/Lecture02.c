637755ab74ca3ca04c62aee8077ade075e65b38f
LeeJeun <gocle11@gmail.com>
2023-04-27 15:44:42 +0900

    0427

--- Lecture02/Lecture02.c
+++ Lecture02/Lecture02.c
@@ -12,16 +12,20 @@ int main()
     //y = 20;
     //z = 3;
 
-    //z = x + y;
+	char c = 65;
+	short s = 200;
+	unsigned int ui = 3000000000U;	// 뒤의 U, L, LL은 생략 가능 (리터럴의 자료형을 표현해주는 기호들)
+	long l = 65537L;
+	long long ll = 12345678908642LL;
 
-    //say_hello();
+	printf("char = %hhd, %d, %c\n", c, c, c);	// %c는 변수를 문자로 출력함
+	printf("short = %hhd, %hd, %d\n", s, s, s);	// hhd(char)는 -128 ~ 127까지 표현 가능. overflow 발생
+	printf("unsigned int = %u, %d\n", ui, ui);	// d(signed int)는 표현 범위가 unsigned int와 달라서 overflow 발생
+	printf("long = %ld, %hd\n", l, l);		// hd(short)는 long보다 표현 범위가 작음 -> overflow
+	printf("long long = %lld, %ld\n", ll, ll);	// long은 long long보다 표현 범위가 작음 -> overflow
 
-    int i = 0, j = 0;
-    scanf("%d%d", &i,&j);
+	return 0;
 
-    printf("value is %d\n", i);
-
-    return 0;
 }
 //void say_hello(void) {
 //    printf("Hello, World!\n");
